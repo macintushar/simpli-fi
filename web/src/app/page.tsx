541c171ae84c8c9890b8cd49d529d8b308744754
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-11-04 22:12:33 +0530

    added basic app with shadcn components

---
+++ web/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div>
+    <h1>Welcome to simpli-fi</h1>
+    <ModeToggle />
+  </div>
+  );
+}
