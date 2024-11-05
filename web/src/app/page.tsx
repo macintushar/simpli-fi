5c3f4e82ace5082d7fe0b45c0bbd92c799032f1f
Tushar Selvakumar <tusharkumar91111@gmail.com>
2024-11-06 04:50:46 +0530

    fixed build error and updated homepage

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,48 +1,16 @@
 import Link from "next/link";
 
-import { LatestPost } from "@/app/_components/post";
 import { auth } from "@/server/auth";
-import { api, HydrateClient } from "@/trpc/server";
-import { GetGroupById } from "./_components/group";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
 
-  if (session?.user) {
-    void api.post.getLatest.prefetch();
-  }
-
   return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-neutral-950 text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight">Simpli-Fi</h1>
-          <h3 className="text-xl font-bold tracking-tight">
-            The simplest finance app for all your group transactions.
-          </h3>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
-
-            <div className="flex flex-col items-center justify-center gap-4">
-              <p className="text-center text-2xl text-white">
-                {session && <span>Logged in as {session.user?.name}</span>}
-              </p>
-              <Link
-                href={session ? "/api/auth/signout" : "/api/auth/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-              >
-                {session ? "Sign out" : "Sign in"}
-              </Link>
-            </div>
-          </div>
-
-          {session?.user && <LatestPost />}
-          {session?.user && <GetGroupById />}
-        </div>
-      </main>
-    </HydrateClient>
+    <div className="flex h-full w-full flex-col items-center">
+      <h1 className="text-5xl font-extrabold tracking-tight">Simpli-Fi</h1>
+      <h3 className="text-xl font-bold tracking-tight">
+        The simplest finance app for all your group transactions.
+      </h3>
+    </div>
   );
 }
